e59a702d0d024c20b1c3facc5ee622cbe3d7cad5
o.ganzienko <ganzienko@gmail.com>
2023-10-29 04:08:18 +0200

    правки

--- address_book.py
+++ address_book.py
@@ -1,57 +1,64 @@
-import json
-
-class Phone:
-    def __init__(self, number):
-        self.number = number
-
-    def __str__(self):
-        return self.number
-
-class Record:
-    def __init__(self, name, phone):
-        self.name = name
-        self.phone = Phone(phone)
-
-    def __str__(self):
-        return f"{self.name}: {self.phone}"
+import datetime
+import pickle
+from record import Record
+from birthday import Birthday
 
 class AddressBook:
+
     def __init__(self):
         self.records = []
-        self.load_data()
-
-    def load_data(self):
-        try:
-            with open("data.json", "r") as f:
-                data = json.load(f)
-                for item in data:
-                    record = Record(item['name'], item['phone'])
-                    self.records.append(record)
-        except FileNotFoundError:
-            pass
-
-    def save_data(self):
-        data = [{"name": record.name, "phone": str(record.phone)} for record in self.records]
-        with open("data.json", "w") as f:
-            json.dump(data, f)
 
     def add_record(self, record):
         self.records.append(record)
-        self.save_data()
 
     def change_phone(self, name, new_phone):
         for record in self.records:
             if record.name == name:
-                record.phone = Phone(new_phone)
-                self.save_data()
+                record.phone = new_phone
                 return True
         return False
 
-    def find_phone(self, name):
+    def get_phone(self, name):
         for record in self.records:
             if record.name == name:
-                return str(record.phone)
+                return record.phone
         return None
 
-    def show_all_records(self):
-        return "\n".join([str(record) for record in self.records])
+    def all_records(self):
+        return [str(record) for record in self.records]
+
+    def add_birthday_to_contact(self, name, date):
+        for record in self.records:
+            if record.name == name:
+                if Birthday.is_valid(date):
+                    record.birthday = Birthday(date)
+                    return True
+        return False
+
+    def get_birthday(self, name):
+        for record in self.records:
+            if record.name == name and record.birthday:
+                return str(record.birthday)
+        return None
+
+    def get_birthdays_per_week(self):
+        today = datetime.date.today()
+        week_time = datetime.timedelta(days=7)
+        upcoming_birthdays = []
+
+        for record in self.records:
+            if record.birthday:
+                next_birthday = datetime.date(today.year, record.birthday.month, record.birthday.day)
+                if today <= next_birthday <= today + week_time:
+                    upcoming_birthdays.append(str(record))
+
+        return upcoming_birthdays
+
+    # модуль pickle
+    def save_to_file(self, filename="addressbook.pkl"):
+        with open(filename, "wb") as file:
+            pickle.dump(self.records, file)
+
+    def load_from_file(self, filename="addressbook.pkl"):
+        with open(filename, "rb") as file:
+            self.records = pickle.load(file)
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from address_book import AddressBook, Record
+import datetime
+from address_book import AddressBook
+from record import Record
+from phone import Phone
+from birthday import Birthday
 
 def parse_input(user_input):
     cmd, *args = user_input.split()
@@ -7,42 +11,59 @@ def parse_input(user_input):
 
 def main():
     book = AddressBook()
+
     print("Welcome to the assistant bot!")
     while True:
         user_input = input("Enter a command: ")
         command, args = parse_input(user_input)
 
-        if command in ["good bye", "close", "exit"]:
+        if command in ["close", "exit"]:
             print("Good bye!")
             break
-
         elif command == "hello":
             print("How can I help you?")
-
         elif command == "add" and len(args) == 2:
-            record = Record(args[0], args[1])
-            book.add_record(record)
-            print("Contact added.")
-
-        elif command == "change" and len(args) == 2:
-            updated = book.change_phone(args[0], args[1])
-            if updated:
-                print("Contact updated.")
+            if Phone.is_valid(args[1]):
+                book.add_record(Record(args[0], args[1]))
+                print(f"Added {args[0]} with phone number {args[1]}")
             else:
-                print(f"No contact named {args[0]} found.")
-
+                print("Invalid phone number. Please enter a 10-digit number.")
+        elif command == "change" and len(args) == 2:
+            if book.change_phone(args[0], args[1]):
+                print(f"Changed phone number for {args[0]} to {args[1]}")
+            else:
+                print("Error: Contact not found or invalid phone format.")
         elif command == "phone" and len(args) == 1:
-            phone = book.find_phone(args[0])
+            phone = book.get_phone(args[0])
             if phone:
                 print(phone)
             else:
-                print(f"No contact named {args[0]} found.")
-
+                print("Contact not found.")
         elif command == "all":
-            print(book.show_all_records())
-
+            contacts = book.all_records()
+            for contact in contacts:
+                print(contact)
+        elif command == "add-birthday" and len(args) == 2:
+            if book.add_birthday_to_contact(args[0], args[1]):
+                print("Birthday added.")
+            else:
+                print("Error: Contact not found or invalid birthday format.")
+        elif command == "show-birthday" and len(args) == 1:
+            birthday = book.get_birthday(args[0])
+            if birthday:
+                print(birthday)
+            else:
+                print("Error: Contact not found or no birthday set.")
+        elif command == "birthdays":
+            upcoming_birthdays = book.get_birthdays_per_week()
+            if upcoming_birthdays:
+                for record in upcoming_birthdays:
+                    print(record)
+            else:
+                print("No birthdays next week.")
         else:
             print("Invalid command.")
 
 if __name__ == "__main__":
     main()
+    
